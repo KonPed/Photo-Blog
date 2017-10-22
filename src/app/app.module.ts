e13f2adeb0390e3e562175ff1f3577c1440b0db9
Konstantinos <kpaidarakis@gmail.com>
2017-10-22 20:44:14 +0300

    added the FormModule and entry-comment-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { EntryListComponent, EntryComponent, EntryService } from './entries';
+import { EntryListComponent, EntryComponent, EntryService, EntryCommentFormComponent } from './entries';
 import { InMemoryEntryService } from './backend';
 
 @NgModule({
   declarations: [
     AppComponent,
     EntryComponent,
-    EntryListComponent
+    EntryListComponent,
+    EntryCommentFormComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    InMemoryWebApiModule.forRoot(InMemoryEntryService)
+    InMemoryWebApiModule.forRoot(InMemoryEntryService),
+    FormsModule
   ],
   providers: [EntryService],
   bootstrap: [AppComponent]
